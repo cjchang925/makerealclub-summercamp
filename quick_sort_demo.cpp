8f0b879875230bbef5f9d4dc26a1afc375986c04
Chi-Chun Chang <cjchang925@gmail.com>
2022-07-28 10:24:15 +0800

    Add qs_demo_short.cpp

--- quick_sort_demo.cpp
+++ quick_sort_demo.cpp
@@ -33,13 +33,9 @@ void quickSort(int* books, int lowerBound, int upperBound) {
 
 int main() {
     int books[1000] = {5, 1, 3, 2, 4};
-    chrono::high_resolution_clock::time_point beginTime = chrono::high_resolution_clock::now();
     quickSort(books, 0, 1000 - 1);
-    chrono::high_resolution_clock::time_point endTime = chrono::high_resolution_clock::now();
-    chrono::duration<double, std::ratio<1, 1000000>> duration_ms = chrono::duration_cast<chrono::duration<double, ratio<1, 1000000>>>(endTime - beginTime);
-    std::cout << "Quick Sort takes " << duration_ms.count() << " microseconds for sorting 1000 elements\n";
-    /*for (int i = 0; i < ARRAY_LENGTH; i++) {
+    for (int i = 0; i < ARRAY_LENGTH; i++) {
         cout << books[i] << ' ';
-    }*/
+    }
     return 0;
 }
