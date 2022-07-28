3c80e59b7a45c461a14d6f543ab5bc900bddefb4
Chi-Chun Chang <cjchang925@gmail.com>
2022-07-28 11:56:35 +0800

    Modify qs_demo_*.cpp

---
+++ qs_demo_quickSort.cpp
@@ -0,0 +1,25 @@
+#define ARRAY_LENGTH 5
+
+void swap(int& a, int& b);
+
+void quickSort(int books[ARRAY_LENGTH], int lowerBound,
+               int upperBound) {
+  if (lowerBound == upperBound) {  // 代表只有一本書，不需要排序
+    return;                        // 結束遞迴
+  }
+  int toBeComparedNumber = books[upperBound];
+  int smallerNumberIndex = lowerBound;
+  for (int walkingIndex = lowerBound; walkingIndex < upperBound;
+       walkingIndex++) {
+    if (books[walkingIndex] < toBeComparedNumber) {
+      swap(books[walkingIndex], books[smallerNumberIndex]);
+      smallerNumberIndex++;
+    }
+  }
+  swap(books[upperBound], books[smallerNumberIndex]);
+
+  quickSort(books, lowerBound, smallerNumberIndex - 1);
+  quickSort(books, smallerNumberIndex + 1, upperBound);
+}
+
+
--- qs_demo_short.cpp
+++ qs_demo_short.cpp
@@ -1,20 +1,22 @@
 #include <iostream>
+
 #define ARRAY_LENGTH 5
+
 using namespace std;
 
 void quickSort(int books[ARRAY_LENGTH], int lowerBound, int upperBound);
 
-void swap(int& a, int& b) {                  // 交換a和b的值
+void swap(int& a, int& b) {   // 交換a和b的值
     int t = a;
     a = b;
     b = t;
 }
 
 int main() {
-    int books[1000] = {5, 1, 4, 2, 3};       // 宣告代表五本書的Array
-    quickSort(books, 0, ARRAY_LENGTH - 1);   // 排序這五本書
-    for (int i = 0; i < ARRAY_LENGTH; i++) {
-        cout << books[i] << ' ';
+    int books[ARRAY_LENGTH] = {5, 1, 4, 2, 3};    // 宣告代表五本書的Array
+    quickSort(books, 0, ARRAY_LENGTH - 1);        // 排序這五本書
+    for (int i = 0; i < ARRAY_LENGTH; i++) {      // 印出排好的五本書
+        cout << books[i] << " ";
     }
     return 0;
 }
